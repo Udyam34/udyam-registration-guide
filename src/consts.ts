86396c91bcbdc45bf6c2de48d464428436c4467d
Rajesh Bhatt <udyam34@gmail.com>
2026-06-23 19:25:36 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Udyam & MSME Guide";
+export const SITE_DESCRIPTION = "Practical guides to Udyam registration, MSME certificates, and related processes in India.";
